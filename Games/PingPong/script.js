be73893b3a09e1454134119aa442399c7cb79267
sandrp <sandro.bohmer@outlook.de>
2023-12-19 16:43:02 +0100

    fixed controlls and design

--- Games/PingPong/script.js
+++ Games/PingPong/script.js
@@ -4,18 +4,18 @@ document.addEventListener('DOMContentLoaded', function () {
     const ball = document.getElementById('ball');
   
     document.addEventListener('keydown', function (event) {
-        // Player 1 controls
-        if (event.key === 'w' && parseInt(paddle1.style.top) > 0) {
-            paddle1.style.top = `${parseInt(paddle1.style.top) - 10}px`;
-        } else if (event.key === 's' && parseInt(paddle1.style.top) < 300) {
-            paddle1.style.top = `${parseInt(paddle1.style.top) + 10}px`;
-        }
-    
-        // Player 2 controls
-        if (event.key === 'ArrowUp' && parseInt(paddle2.style.top) > 0) {
-            paddle2.style.top = `${parseInt(paddle2.style.top) - 10}px`;
-        } else if (event.key === 'ArrowDown' && parseInt(paddle2.style.top) < 300) {
-            paddle2.style.top = `${parseInt(paddle2.style.top) + 10}px`;
-        }
+      // Player 1 controls
+      if (event.key === 'w' && parseInt(paddle1.style.top) > 0) {
+        paddle1.style.top = `${parseInt(paddle1.style.top) - 10}px`;
+      } else if (event.key === 's' && parseInt(paddle1.style.top) < 300) {
+        paddle1.style.top = `${parseInt(paddle1.style.top) + 10}px`;
+      }
+  
+      // Player 2 controls
+      if (event.key === 'ArrowUp' && parseInt(paddle2.style.top) > 0) {
+        paddle2.style.top = `${parseInt(paddle2.style.top) - 10}px`;
+      } else if (event.key === 'ArrowDown' && parseInt(paddle2.style.top) < 300) {
+        paddle2.style.top = `${parseInt(paddle2.style.top) + 10}px`;
+      }
     });
-});
+  });
