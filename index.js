0d31f3b47929fcc9a4f2f34cfafd56d57e4d899e
Sandro <sandro.bohmer@outlook.de>
2024-07-26 11:19:30 +0200

    changed overall theme and added in the new website.

---
+++ index.js
@@ -0,0 +1,8 @@
+function showSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'flex';
+}
+function hideSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'none';
+}
